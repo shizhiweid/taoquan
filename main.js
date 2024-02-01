9f4a2b67e3175ff2c0fb1f82a1bb761feda6e476
shi <1613285977@qq.com>
2024-02-01 08:29:42 +0800

    init

---
+++ main.js
@@ -0,0 +1,25 @@
+import App from './App'
+import uView from "uview-ui";
+// #ifndef VUE3
+import Vue from 'vue'
+import './uni.promisify.adaptor'
+Vue.config.productionTip = false
+App.mpType = 'app'
+Vue.use(uView);
+const app = new Vue({
+	...App
+})
+app.$mount()
+// #endif
+
+// #ifdef VUE3
+import {
+	createSSRApp
+} from 'vue'
+export function createApp() {
+	const app = createSSRApp(App)
+	return {
+		app
+	}
+}
+// #endif
---
+++ pages/index/index.vue
@@ -0,0 +1,47 @@
+<template>
+	<view class="">
+		<u-form :model="form" ref="uForm">
+			<u-form-item label="姓名">
+				<u-input v-model="form.name" />
+			</u-form-item>
+			<u-form-item label="简介">
+				<u-input v-model="form.intro" />
+			</u-form-item>
+			<u-form-item label="性别">
+				<u-input v-model="form.sex" type="select" />
+			</u-form-item>
+			<u-form-item label="水果">
+				<u-checkbox-group width="50%">
+					<u-checkbox>苹果</u-checkbox>
+					<u-checkbox>雪梨</u-checkbox>
+					<u-checkbox>柠檬</u-checkbox>
+					<u-checkbox>橘子</u-checkbox>
+				</u-checkbox-group>
+			</u-form-item>
+			<u-form-item label="味道">
+				<u-radio-group>
+					<u-radio>鲜甜</u-radio>
+					<u-radio>麻辣</u-radio>
+				</u-radio-group>
+			</u-form-item>
+			<u-form-item label="开关">
+				<u-switch slot="right"></u-switch>
+			</u-form-item>
+		</u-form>
+	</view>
+
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				form: {
+					name: '',
+					intro: '',
+					sex: ''
+				}
+			}
+		},
+	}
+</script>
